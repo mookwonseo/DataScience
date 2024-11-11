76dfa2ff296a53e8bc2800bcb3107a7a7a0346f9
Mookwon Seo <mookwonseo@Mookwons-Laptop.local>
2024-11-11 12:55:37 -0700

    Need to fix ordinal_encoder

--- DataScience.py
+++ DataScience.py
@@ -72,7 +72,6 @@ numerical_cols = [col for col in X_train_full.columns if X_train_full[col].dtype
 my_cols = categorical_cols + numerical_cols
 
 # Preprocessing for numerical data using Scikit Learn
-numerical_trainsformer = SimpleImputer(strategy="constant")
 
 cat_X_train = X_train_full[categorical_cols]
 cat_X_valid = X_valid_full[categorical_cols]
@@ -83,22 +82,23 @@ missing_val_counts = num_X_train.isnull().sum()
 missing_val_columns = num_X_train.columns[missing_val_counts>0]
 
 # Basic idea of missing values from numerical data
-missingValCntrlOption = 2
+missingValCntrlOption = 3
+strategy="constant"
 try: 
     if missingValCntrlOption == 1:
-        my_X_train = num_X_train.drop(missing_val_columns, axis = 1) # axis = 0: index, axis = 1: columns
-        my_X_valid = num_X_valid.drop(missing_val_columns, axis = 1)
+        my_X_train_num = num_X_train.drop(missing_val_columns, axis = 1) # axis = 0: index, axis = 1: columns
+        my_X_valid_num = num_X_valid.drop(missing_val_columns, axis = 1)
         out_string = "Drop Missing Values"
 
     elif missingValCntrlOption == 2:
-        myimputer = SimpleImputer() #default: mean
-        my_X_train = pd.DataFrame(myimputer.fit_transform(num_X_train))
-        my_X_valid = pd.DataFrame(myimputer.transform(num_X_valid))
+        myimputer = SimpleImputer(strategy=strategy) #default: mean
+        my_X_train_num = pd.DataFrame(myimputer.fit_transform(num_X_train))
+        my_X_valid_num = pd.DataFrame(myimputer.transform(num_X_valid))
         
-        my_X_train.columns = num_X_train.columns
-        my_X_valid.columns = num_X_valid.columns
-        my_X_train.index = num_X_train.index
-        my_X_valid.index = num_X_valid.index
+        my_X_train_num.columns = num_X_train.columns
+        my_X_valid_num.columns = num_X_valid.columns
+        my_X_train_num.index = num_X_train.index
+        my_X_valid_num.index = num_X_valid.index
         out_string = "Imputed Missing Values"
 
         ## Other ways to relace the nan to the mean values
@@ -111,38 +111,100 @@ try:
         # temp_num_X_valid[missing_val_columns] = num_X_valid[missing_val_columns].replace(np.nan, mean_train)
         # print(temp_num_X_train.compare(my_X_train))
         # print(temp_num_X_valid.compare(my_X_valid))
-        
-        
-
+    
     elif missingValCntrlOption == 3:
-        X_train_plus = X_train_full.copy()
-        X_valid_plus = X_valid_full.copy()
+        X_train_plus = num_X_train.copy()
+        X_valid_plus = num_X_valid.copy()
         
         for col in missing_val_columns:
             X_train_plus[col+"_missing_vals"]=X_train_plus[col].isnull()
             X_valid_plus[col+"_missing_vals"]=X_valid_plus[col].isnull()
         
-        myimputer = SimpleImputer()
-        my_X_train = pd.DataFrame(myimputer.fit_transform(X_train_plus))
-        my_X_valid = pd.DataFrame(myimputer.transform(X_valid_plus))
+        myimputer = SimpleImputer(strategy=strategy) #default: mean
+        my_X_train_num = pd.DataFrame(myimputer.fit_transform(X_train_plus))
+        my_X_valid_num = pd.DataFrame(myimputer.transform(X_valid_plus))
         
-        my_X_train.columns = X_train_plus.columns
-        my_X_valid.columns = X_valid_plus.columns
+        my_X_train_num.columns = X_train_plus.columns
+        my_X_valid_num.columns = X_valid_plus.columns
         
+        my_X_train_num.index = X_train_plus.index
+        my_X_valid_num.index = X_valid_plus.index
+
         out_string = "Extended Imputed Missing Values"
     else:
         raise KeyboardInterrupt("Out of range")
 finally:
     pass
 
+# Basic idea of missing values from categorical data
+# Check if some categorical names in test data is not in test data
+# I didn't import test data set yet. So I am testing with valid set. 
+good_label_cols = [col for col in categorical_cols if set(X_valid_full[col]).issubset(X_train_full[col])]
+bad_label_cols = set(X_train_full[categorical_cols])-set(X_valid_full[good_label_cols])
+
+label_X_train = cat_X_train.drop(bad_label_cols, axis = 1)
+label_X_valid = cat_X_valid.drop(bad_label_cols, axis = 1)
+
+print(X_train_full[categorical_cols])
+
+objColumnCntrlOption = 2
+try:
+    if objColumnCntrlOption == 1:
+        ### drop the whole columns
+        my_X_train_cat = pd.DataFrame()
+        my_X_valid_cat = pd.DataFrame()
+    elif objColumnCntrlOption == 2:
+        ## Need to fix it
+        ordinal_encoder = OrdinalEncoder() # Your code here
+        label_X_train[good_label_cols] = ordinal_encoder.fit_transform(label_X_train[good_label_cols])
+        label_X_valid[good_label_cols] = ordinal_encoder.transform(label_X_valid[good_label_cols])
+
+        my_X_train_cat = label_X_train
+        my_X_valid_cat = label_X_train
+    elif objColumnCntrlOption == 3:
+        low_cardinality_cols = [col for col in good_label_cols if label_X_train[col].nunique() < 10]
+        high_cardinality_cols = list(set(good_label_cols)-set(low_cardinality_cols))
+
+        OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+        my_X_train_cat = pd.DataFrame(OH_encoder.fit_transform(label_X_train[low_cardinality_cols]))
+        my_X_valid_cat = pd.DataFrame(OH_encoder.transform(label_X_valid[low_cardinality_cols]))
+
+        my_X_train_cat.index = label_X_train.index
+        my_X_valid_cat.index = label_X_valid.index
+
+        my_X_train_cat.columns = label_X_train.columns
+        my_X_valid_cat.columns = label_X_valid.columns
+    elif objColumnCntrlOption == 4:
+        ### Simple imputer most frequent
+        print("Not Implemented")
+    else:
+        raise KeyboardInterrupt("Out of range")
+finally:
+    pass
 
 
+#print(my_X_train_cat)
+
+
+
+filtered_X_train = pd.concat([my_X_train_num, my_X_train_cat ], axis = 1)
+filtered_X_valid = pd.concat([my_X_valid_num, my_X_valid_cat ], axis = 1)
+
+print(score_dataset(filtered_X_train, filtered_X_valid, y_train, y_valid))
+
+############################################################
+############### Using PipeLine #############################
+############################################################
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+# Preprocessing for numerical data
+numerical_trainsformer = SimpleImputer(strategy="constant")
 # Preprocessing for categorical data
 categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_mfrequent')),
+    ('imputer', SimpleImputer(strategy='most_frequent')),
     ('onehot', OneHotEncoder(handle_unknown='ignore'))
 ])
-
 # Bundle preprocessing for numerical and categorical data
 preprocessor = ColumnTransformer(
     transformers=[
@@ -150,3 +212,16 @@ preprocessor = ColumnTransformer(
         ('cat',categorical_transformer, categorical_cols)
     ]
 )
+# Define model
+model=RandomForestRegressor(n_estimators=100, random_state=0)
+# Bundle preprocessing and modeling code in a pipeline
+clf = Pipeline(steps=[
+    ('preprocess', preprocessor),
+    ('model',model)
+])
+
+# Preprocessing of training data, fit model 
+clf.fit(X_train, y_train)
+preds = clf.predict(X_valid)
+
+print('MAE:', mean_absolute_error(y_valid, preds))
